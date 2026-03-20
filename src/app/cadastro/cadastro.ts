f8d5d53ab12dd57f8756544d1c39b4a101dc017b
Danyel França <danyelfranca.vasco@gmail.com>
2026-03-20 09:56:17 -0300

    quarto commit

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
+
 export class Cadastro {
+
+passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+
+form = new FormGroup({
+  nome : new FormControl('', [Validators.required]),
+  email : new FormControl('', [Validators.required, Validators.email]),
+  senha : new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex)])
+});
+
+validatePassword() {
+  const password = this.form.get('senha')?.value ?? '';
+  const isValid = this.passwordRegex.test(password);
+  return isValid;
+}
+
+onSubmit(){
+  if(this.form.valid)
+    console.log("Formulário válido:", this.form.value);
+  else
+    console.log("Formulário inválido:", this.form.value);
+}
+
   regraMinimoCaractere: Boolean = false; // Começa com false, poque o campo de senha começa vazio
   regraMaximoCaractere: Boolean = false;
   regraLetraMaiuscula: Boolean = false;
